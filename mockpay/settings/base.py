aa8e94128ad5a6be24d7d06e1b6e60014a231b35
CM Lubinski <christopher.lubinski@gsa.gov>
2014-09-18 19:58:15 +0000

    Display a 'credit card' form

--- mockpay/settings/base.py
+++ mockpay/settings/base.py
@@ -2,11 +2,19 @@
 MIDDLEWARE_CLASSES = []
 APPEND_SLASH = False
 ROOT_URLCONF = "mockpay.urls"
+INSTALLED_APPS = [
+    "mockpay",
+]
 
 # Custom settings,
 
-CALLBACK_INFO = {}
-# Format: {agency_id: {
-#                      app_name: {"form_id": an_int, "url": a_url},
-#                      "DEFAULT": {"form_id": an_int, "url": a_url}
+CALLBACK_URLS = {}
+# Format: {agency_id: {app_name: a_url,
+#                      "DEFAULT": a_default_url
 #         }}
+
+FORM_CONFIGS = {}
+# We don't know how forms are configured yet, so this is just a best guess.
+# Map forms to a list of fields
+# Format: {form_id: [{"name": field_name, "status": "editable|locked|hidden"},
+#                    ...]}
--- mockpay/tests.py
+++ mockpay/tests.py
@@ -6,7 +6,8 @@ from mockpay import views
 
 
 class ViewsTests(SimpleTestCase):
-    def test_entry_requirements(self):
+    @patch('mockpay.views.requests')
+    def test_entry_requirements(self, requests):
         """Entry point only accepts POSTs, and needs two fields set"""
         response = self.client.get(reverse('entry'))
         self.assertEqual(405, response.status_code)
@@ -23,32 +24,93 @@ class ViewsTests(SimpleTestCase):
         response = self.client.post(reverse('entry'), message)
         self.assertEqual(400, response.status_code)
 
+        #   None of the tests thus far should have made an http request
+        self.assertFalse(requests.post.called)
+
     @patch('mockpay.views.requests')
     def test_entry_requirements_valid(self, requests):
         """A successful POST from the browser should result in a POST to the
         configured url with the provided agency_/tracking_id"""
-        mock_info = {"AGENCY": {"DEFAULT": {"form_id": 1, "url": "exexex"}}}
-        with self.settings(CALLBACK_INFO=mock_info):
+        mock_info = {"AGENCY": {"DEFAULT": "exexex"}}
+        with self.settings(CALLBACK_URLS=mock_info):
             message = {'agency_id': 'AGENCY', 'agency_tracking_id': 'TRA'}
-            response = self.client.post(reverse('entry'), message)
-            self.assertEqual(200, response.status_code)
+            self.client.post(reverse('entry'), message)
             self.assertTrue(requests.post.called)
             self.assertEqual(requests.post.call_args[0][0], 'exexex')
             self.assertEqual(requests.post.call_args[1]['data'], message)
 
-    def test_lookup_callback_info(self):
+    def test_lookup_callback_url(self):
         """We should only get callback info if the agency_id and app match, or
         there is no app"""
-        mock_info = {"AGENCY": {
-            "ANAPP": {"form_id": 123, "url": "http://example.com/"},
-            "DEFAULT": {"form_id": 321, "url": "http://example.gov/"}
-        }}
-        with self.settings(CALLBACK_INFO=mock_info):
-            self.assertEqual(views.lookup_callback_info("other_agency", None),
+        mock_info = {"AGENCY": {"ANAPP": "http://example.com/",
+                                "DEFAULT": "http://example.gov/"}}
+        with self.settings(CALLBACK_URLS=mock_info):
+            self.assertEqual(views.lookup_callback_url("other_agency", None),
                              None)
-            self.assertEqual(views.lookup_callback_info("AGENCY", "NONAPP"),
+            self.assertEqual(views.lookup_callback_url("AGENCY", "NONAPP"),
                              None)
-            self.assertEqual(views.lookup_callback_info("AGENCY", "ANAPP"),
-                             {"form_id": 123, "url": "http://example.com/"})
-            self.assertEqual(views.lookup_callback_info("AGENCY", None),
-                             {"form_id": 321, "url": "http://example.gov/"})
+            self.assertEqual(views.lookup_callback_url("AGENCY", "ANAPP"),
+                             "http://example.com/")
+            self.assertEqual(views.lookup_callback_url("AGENCY", None),
+                             "http://example.gov/")
+
+    def test_clean_agency_response_errors(self):
+        """This function should return error strings if given XML, duplicate
+        keys, missing value, unknown keys, or not given a required key"""
+        inputs = ["<xml>",
+                  "key=value\nkey=other value",
+                  "unexplained string",
+                  "key=   ",
+                  "action=value\npayment_type=value",
+                  "protocol_version=1\nresponse_message=2\naction=3\n"
+                  + "form_id=4\nagency_tracking_id=5\nother_key=6"]
+        responses = set()
+        for response_str in inputs:
+            clean = views.clean_agency_response(response_str)
+            self.assertTrue(isinstance(clean, str))
+            responses.add(clean)
+        # they should all have provided different error messages
+        self.assertEqual(len(responses), len(inputs))
+
+    def test_clean_agency_response(self):
+        """Example of a successful input-string-to-dictionary conversion as
+        all required (and some optional) fields are present. Tests various
+        line endings"""
+        input_str = ("action=SubmitCollectionInteractive\n"
+                     + "payment_amount=20.25\r\n"
+                     + "form_id=123\r"
+                     + "payer_name=Bob Smith\n"
+                     + "agency_tracking_id=090909\n"
+                     + "protocol_version=3.2\n"
+                     + "response_message=Success")
+        clean = views.clean_agency_response(input_str)
+        self.assertEqual(clean, {
+            "action": "SubmitCollectionInteractive",
+            "payment_amount": "20.25",
+            "form_id": "123",
+            "payer_name": "Bob Smith",
+            "agency_tracking_id": "090909",
+            "protocol_version": "3.2",
+            "response_message": "Success"
+        })
+
+    def test_generate_form_no_form(self):
+        """The form is looked up; if it's not present, we get an error"""
+        with self.settings(FORM_CONFIGS={"111": []}):
+            response = views.generate_form(None, {"form_id": "2222"})
+            self.assertEqual(response.status_code, 400)
+
+    def test_generate_form(self):
+        """The generated form should include only fields in the form config"""
+        config = {"111": [{"name": "field1", "status": "editable"},
+                          {"name": "field2", "status": "locked"},
+                          {"name": "field3", "status": "hidden"}]}
+        params = {"form_id": "111", "field2": "value2", "field4": "value4"}
+        with self.settings(FORM_CONFIGS=config):
+            response = views.generate_form(None, params)
+            self.assertContains(response, "field1")
+            self.assertContains(response, "field2")
+            self.assertContains(response, "field3")
+            self.assertContains(response, "value2")
+            self.assertNotContains(response, "field4")
+            self.assertNotContains(response, "value4")
